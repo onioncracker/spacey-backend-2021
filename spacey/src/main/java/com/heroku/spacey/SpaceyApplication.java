c469fc6c8a4dec2ba25a65f325131660a1bd6400
Katatyunyaa <84133989+Katatyunyaa@users.noreply.github.com>
2021-05-13 20:07:48 +0300

    Add files via upload

---
+++ spacey/src/main/java/com/heroku/spacey/SpaceyApplication.java
@@ -0,0 +1,13 @@
+package com.heroku.spacey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceyApplication.class, args);
+    }
+
+}
