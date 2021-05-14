8bb24e87e8dd22f447bacbda8759ca2820eb7ae1
DaLv1ko <vlad1602dubno2552dalv@gmail.com>
2021-05-14 17:34:14 +0300

    test hello controller

---
+++ src/main/java/com/heroku/spacey/TestController.java
@@ -0,0 +1,14 @@
+package com.heroku.spacey;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "onion";
+    }
+
+}
