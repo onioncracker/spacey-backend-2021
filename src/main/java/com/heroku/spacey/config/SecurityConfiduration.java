8282c0d1d82e0b39f471412e02efa53637a5db31
arkile <lunkans@ukr.net>
2021-05-17 15:32:40 +0300

    misc file fix

--- src/main/java/com/heroku/spacey/config/SecurityConfiduration.java
+++ src/main/java/com/heroku/spacey/config/SecurityConfiduration.java
@@ -8,11 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.stereotype.Component;
 
 @Configuration
 @EnableWebSecurity
