ca539ddb6074a66418c08f89775b99447e1058b0
arkile <lunkans@ukr.net>
2021-05-28 18:33:41 +0300

    email and password validators added

---
+++ src/main/java/com/heroku/spacey/utils/PasswordValidator.java
@@ -0,0 +1,13 @@
+package com.heroku.spacey.utils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordValidator implements ConstraintValidator<PasswordConstraint, String> {
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext cxt) {
+        return password != null && password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$")
+                && (password.length() > 8) && (password.length() < 24);
+    }
+}
