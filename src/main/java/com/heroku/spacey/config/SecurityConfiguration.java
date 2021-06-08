85c48a0e8165153920c7259adcdf081722cf8e17
arkile <lunkans@ukr.net>
2021-06-08 10:20:37 +0300

    cors fixes

--- src/main/java/com/heroku/spacey/config/SecurityConfiguration.java
+++ src/main/java/com/heroku/spacey/config/SecurityConfiguration.java
@@ -14,8 +14,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
