e3e61b6b8aa67cabf407b5f4ee6b7ecd55f3791e
OleksandrShevchuk <legolasokay@gmail.com>
2021-06-22 16:29:08 +0300

    Minor checkout fixes.

--- src/main/java/com/heroku/spacey/controllers/CheckoutController.java
+++ src/main/java/com/heroku/spacey/controllers/CheckoutController.java
@@ -3,14 +3,16 @@ package com.heroku.spacey.controllers;
 import lombok.RequiredArgsConstructor;
 import com.heroku.spacey.dto.order.CheckoutDto;
 import com.heroku.spacey.services.CheckoutService;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/checkout")
+@Secured("ROLE_USER")
 @RequiredArgsConstructor
+@RequestMapping("/api/checkout")
 public class CheckoutController {
     private final CheckoutService checkoutService;
 
