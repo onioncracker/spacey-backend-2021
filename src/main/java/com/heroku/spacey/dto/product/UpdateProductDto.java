3136e19530282aa11f4c36e3dd48f4d3315c27b8
Dmytro <dima.tumochko@gmail.com>
2021-06-24 15:26:38 +0300

    Fix UpdateProductDto, ProductServiceImpl

--- src/main/java/com/heroku/spacey/dto/product/UpdateProductDto.java
+++ src/main/java/com/heroku/spacey/dto/product/UpdateProductDto.java
@@ -4,7 +4,7 @@ import com.heroku.spacey.dto.category.CategoryDto;
 import com.heroku.spacey.dto.color.ColorDto;
 import com.heroku.spacey.dto.material.MaterialDto;
 import java.math.BigDecimal;
-import java.sql.Date;
+import java.util.List;
 
 import com.heroku.spacey.dto.size.SizeDto;
 import lombok.Data;
@@ -12,7 +12,6 @@ import lombok.Data;
 @Data
 public class UpdateProductDto {
     private String name;
-    private Date createdDate;
     private String productSex;
     private BigDecimal price;
     private Double discount;
@@ -20,7 +19,7 @@ public class UpdateProductDto {
     private String description;
     private Boolean isAvailable;
     private CategoryDto category;
-    private MaterialDto material;
-    private SizeDto size;
     private ColorDto color;
+    private List<MaterialDto> materials;
+    private List<SizeDto> sizes;
 }
