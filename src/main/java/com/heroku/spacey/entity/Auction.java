fc53a359ba5271a6a35ac638078123295fed6666
Dmytro <dima.tumochko@gmail.com>
2021-06-24 11:34:49 +0300

    Migrate entity to new database

--- src/main/java/com/heroku/spacey/entity/Auction.java
+++ src/main/java/com/heroku/spacey/entity/Auction.java
@@ -2,21 +2,29 @@ package com.heroku.spacey.entity;
 
 import lombok.Data;
 
-import java.sql.Timestamp;
+import java.sql.Date;
 
 @Data
 public class Auction {
     private Long auctionId;
 
-    private User auctionUser;
+    private User auctionBuyer;
     private Long userId;
 
+    private Product actionProduct;
+    private Long productId;
+
+    private Size productSize;
+    private Long sizeId;
+
+    private Integer amount;
+    private Long packageAmount;
     private String auctionName;
     private Boolean auctionType;
     private Double startPrice;
     private Double endPrice;
     private Double priceStep;
-    private Timestamp startTime;
-    private Timestamp endTime;
+    private Date startTime;
+    private Date endTime;
     private String status;
 }
