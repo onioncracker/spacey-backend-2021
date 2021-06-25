1d88976d130f67b20911e434b76989f3451ac602
Dmytro <dima.tumochko@gmail.com>
2021-06-25 22:57:10 +0300

    Sonarcloud duplicate code fix

--- src/main/java/com/heroku/spacey/dto/product/UpdateProductDto.java
+++ src/main/java/com/heroku/spacey/dto/product/UpdateProductDto.java
@@ -4,6 +4,7 @@ import com.heroku.spacey.dto.category.CategoryDto;
 import com.heroku.spacey.dto.color.ColorDto;
 import com.heroku.spacey.dto.material.MaterialDto;
 import java.math.BigDecimal;
+import java.sql.Date;
 import java.util.List;
 
 import com.heroku.spacey.dto.size.SizeDto;
@@ -12,6 +13,7 @@ import lombok.Data;
 @Data
 public class UpdateProductDto {
     private String name;
+    private Date createdDate;
     private String productSex;
     private BigDecimal price;
     private Double discount;
