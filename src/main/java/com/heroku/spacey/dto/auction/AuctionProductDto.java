ff57ee13cc0d07441ec406c29f9ff6649d52fe62
Dmytro <dima.tumochko@gmail.com>
2021-07-01 22:44:50 +0300

    Change model for productAuction.

--- src/main/java/com/heroku/spacey/dto/auction/AuctionProductDto.java
+++ src/main/java/com/heroku/spacey/dto/auction/AuctionProductDto.java
@@ -1,8 +1,19 @@
 package com.heroku.spacey.dto.auction;
 
+import com.heroku.spacey.dto.category.CategoryDto;
+import com.heroku.spacey.dto.color.ColorDto;
+import com.heroku.spacey.dto.material.MaterialDto;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class AuctionProductDto {
     private Long id;
+    private String name;
+    private String description;
+    private String productSex;
+    private CategoryDto productCategory;
+    private ColorDto productColor;
+    private List<MaterialDto> materials;
 }
